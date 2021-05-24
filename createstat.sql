313ae8702d08fab7c1cbef2e6772cef4e78eaa97
Sivaram Konanki <sivaram.konanki@rakuten.com>
2021-05-24 18:44:44 +0900

    init commit

---
+++ createstat.sql
@@ -0,0 +1,29 @@
+-- creates a separate stats database based on the data in the dbstats virtual table (used to get the page read log in the blog post)
+attach 'wdi.sqlite3' as wdi;
+
+create table names (id integer primary key, name text not null);
+create table pagetypes (id integer primary key, name text not null);
+
+insert into names (name) select distinct name from dbstat('wdi');
+insert into pagetypes (name) select distinct pagetype from dbstat('wdi');
+
+create table stat (
+    pageno integer primary key,
+    name references names (id) not null,
+    pagetype references pagetypes (id) not null,
+    number_of_cells integer not null,
+    payload_bytes integer not null,
+    unused_bytes integer not null
+);
+
+insert into stat select
+    pageno,
+    (select id from names where names.name = s.name),
+    (select id from pagetypes where pagetypes.name = s.pagetype),
+    ncell,
+    payload,
+    unused
+    from dbstat('wdi') s;
+
+vacuum;
+
